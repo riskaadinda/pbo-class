1bbc23d6f9f3e4dfe15d438277ff90daf2cadaa1
riskaadinda <84611327+riskaadinda@users.noreply.github.com>
2021-06-16 11:23:44 +0700

    Add files via upload

---
+++ NewClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class NewClass {
+    <?php
+Function SetNilai($nilai = 80)
+{
+	echo $nilai;
+}
+SetNilai();
+?>
+}
